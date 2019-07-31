84ed0c7b7e15dde68a89a40b9a14f2ff5493d166
lucas nascimento <lucaspnascimento@hotmail.com>
2019-07-31 10:50:26 -0300

    att API node

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,38 @@
+import User from '../models/Users';
+import Notification from '../schemas/notification';
+
+class NotificationController {
+  // eslint-disable-next-line class-methods-use-this
+  async index(req, res) {
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only providers can load notifications' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'eb9e6c3ed51473',
+    pass: '949ac73a83fc47',
+  },
+  default: {
+    from: 'Equipe BarberApp <noreply@apapbarber.com>',
+  },
+};
